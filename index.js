421bab19cd56282c288d36049506485ef450925b
sejal-kale <75160356+sejal-kale@users.noreply.github.com>
2023-04-01 03:14:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+
+const routes=require('./routes/ToDoRoute')
+
+require('dotenv').config()
+
+const app=express()
+const PORT=process.env.port || 5000
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=>console.log(`Connected To MongoDB.....`))
+.catch((err)=>console.log(err))
+
+app.use(express.json())
+app.use(cors())
+
+
+app.use(routes)
+
+app.listen(PORT ,()=>console.log(`Listening on: ${PORT}`))
